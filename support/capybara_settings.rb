956053e0c7561d2994a83de5fdfa594482a796e6
保木本将之 <markun@markun.local>
2017-09-05 23:20:30 +0900

    Remove useragent setting.

--- support/capybara_settings.rb
+++ support/capybara_settings.rb
@@ -8,7 +8,7 @@ Capybara.register_driver :selenium do |app|
     browser: :chrome,
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
       chrome_options: {
-        args: %w[window-size=1600,800 headless disable-gpu no-sandbox user-agent=Mozilla/5.0\ (Macintosh;\ Intel\ Mac\ OS\ X\ 10.12;\ rv:54.0)\ Gecko/20100101\ Firefox/54.0]
+        args: %w[window-size=1600,800 headless disable-gpu no-sandbox]
       }
     )
   )
